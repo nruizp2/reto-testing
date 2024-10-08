031018c3e88f9c16488d36c0873f41ffdd2900f0
nruizp2 <n.ruizp2@uniandes.edu.co>
2024-10-08 11:35:02 -0500

    a

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import Checkbox from './components/checkbox';
+import Like from './components/like';
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<Checkbox labelActive="Active" labelInactive="Inactive" />)
+root.render(<div>
+    <Checkbox labelActive="Active" labelInactive="Inactive" />
+    <Like/>
+</div>)
